e73335f98cb44fc63e8a5620d3d9622fcf01c192
ravil-mobile <ravil.aviva.com@gmail.com>
2023-11-28 12:08:23 +0000

    Added GraphModifier class (i.e., a part of binary fusion)

--- src/hip_graph_modifier.hpp
+++ src/hip_graph_modifier.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <cstddef>
+#include <memory>
 #include <unordered_map>
-#include <utility>
 #include <vector>
-#include "hip/hip_runtime.h"
-#include "hip/hip_runtime_api.h"
 #include "hip_graph_internal.hpp"
-#include "platform/kernel.hpp"
+#include "hip_code_object.hpp"
 
 
 namespace hip {
@@ -15,18 +12,37 @@ class GraphModifier {
   amd::Monitor fclock_{"Guards Graph Modifier object", true};
 
  public:
-  GraphModifier(hipGraph_t& graph) : graph_(graph) {};
+  GraphModifier(hipGraph_t& graph);
+  ~GraphModifier();
   void run();
   static bool isSubstitutionOn();
 
-  private:
+ private:
+  struct GraphDescription {
+    std::vector<std::string> groupSymbols_{};
+    std::vector<std::vector<size_t>> executionGroups_{};
+    std::vector<hipGraphNode*> fusionGroups_{};
+  };
+
   static bool isInputOk();
+  bool check();
+  std::vector<Node> collectNodes(const std::vector<Node>& originalNodes);
+  void generateFusedNodes();
+  void adjustNodeLevels();
 
   hipGraph_t& graph_;
+  size_t instanceId_{};
+  GraphDescription currDescription{};
 
+  struct Finalizer {
+    void operator()(size_t*) const;
+  };
+  using CounterType = std::unique_ptr<size_t, Finalizer>;
+
+  static CounterType instanceCounter_;
   static bool isSubstitutionStateQueried_;
   static bool isSubstitutionSwitchedOn_;
-  static std::unordered_map<std::string, amd::Kernel*> symbolTable_;
-  static std::vector<std::vector<size_t>> executionOrder_;
+  static hip::ExternalCOs::SymbolTableType symbolTable_;
+  static std::vector<GraphDescription> descriptions_;
 };
 }  // namespace hip
